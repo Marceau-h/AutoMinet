7b434ad5a79a8ba5f438fd779e455b6a6fb19e9c
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2023-07-07 12:32:45 +0200

    update bash

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 # echo "$@"
 echo "Installation des dépendances..."
-python3 -m pip install --upgrade pip > /dev/null
-python3 -m pip install -r requirements.txt > /dev/null
+python3 -m pip install --upgrade pip > /dev/null || exit_on_error
+python3 -m pip install -r requirements.txt > /dev/null || exit_on_error
 
 echo "Vérification des cookies..."
-OUTPUT="$(python3 minetcookies.py "$@")"
+OUTPUT="$(python3 minetcookies.py "$@")" || exit_on_error
 # echo "$OUTPUT"
 
 echo "Récupération des données..."
@@ -13,11 +13,11 @@ minet "$@" > temp.csv
 
 if [ "$1" = "tiktok" ]; then
     echo "Récolte des vidéos..."
-    python3 tksel.py temp.csv "$3"  # --no-headless
-    cp temp.csv "$OUTPUT"/meta.csv
-    pellipop --input "$OUTPUT" --output "$OUTPUT"-pellipop --frequency 1 --remove_duplicates
+    python3 tksel.py temp.csv "$3" || exit_on_error  # --no-headless
+    cp temp.csv "$OUTPUT"/meta.csv || exit_on_error
+    pellipop --input "$OUTPUT" --output "$OUTPUT"-pellipop --frequency 1 --remove_duplicates || exit_on_error
 else
-  echo "$(<temp.csv)"
+  echo "$(<temp.csv)" || exit_on_error
 fi
 
 rm temp.csv
