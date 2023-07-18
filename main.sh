650ed4cf43f860578f7356ea32247066045e6353
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2023-07-18 16:33:56 +0200

    minetcookies as a package !
    + stricter requirements
    + exit when keyboardinterrupt
    + ignore all folders (they contains tests)

--- main.sh
+++ main.sh
@@ -1,23 +1,23 @@
 # echo "$@"
 echo "Installation des dépendances..."
-python3 -m pip install --upgrade pip > /dev/null || exit_on_error
-python3 -m pip install -r requirements.txt > /dev/null || exit_on_error
+python3 -m pip install --upgrade pip > /dev/null || exit
+python3 -m pip install -r requirements.txt > /dev/null || exit
 
 echo "Vérification des cookies..."
-OUTPUT="$(python3 minetcookies.py "$@")" || exit_on_error
+OUTPUT="$(minetcookies "$@")" || exit
 # echo "$OUTPUT"
 
 echo "Récupération des données..."
-minet "$@" -o temp.csv
+minet "$@" -o temp.csv || exit
 # echo "$(<temp.csv)"
 
 if [ "$1" = "tiktok" ]; then
     echo "Récolte des vidéos..."
-    python3 tksel.py temp.csv "$3" || exit_on_error  # --no-headless
-    cp temp.csv "$OUTPUT"/meta.csv || exit_on_error
-    pellipop --input "$OUTPUT" --output "$OUTPUT"-pellipop --frequency 1 --remove_duplicates || exit_on_error
+    python3 tksel.py temp.csv "$3" || exit  # --no-headless
+    cp temp.csv "$OUTPUT"/meta.csv || exit
+    pellipop --input "$OUTPUT" --output "$OUTPUT"-pellipop --frequency 1 --remove_duplicates || exit
 else
-  echo "$(<temp.csv)" || exit_on_error
+  echo "$(<temp.csv)" || exit
 fi
 
 rm temp.csv
